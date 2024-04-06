fef199eec3eff5d60fdaf6223077968c5b217daa
paulo.paes <paulo.paes@wises.com.br>
2024-04-06 14:36:12 -0300

    add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,10 @@ const Home: NextPage = () => {
       <Head>
         <title>Paulo Paes</title>
         <meta name="description" content="Paulo Paes Software Engineering, Node.JS Specialist" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Navbar />
       <Box height="3rem"></Box>
